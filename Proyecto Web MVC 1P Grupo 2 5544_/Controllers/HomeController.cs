48a49f7fe97f0f1b6c50bdafd9c383f94c40f87d
Daniel <emil.encalada@udla.edu.ec>
2025-04-30 07:16:04 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto Web MVC 1P Grupo 2 5544_/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Web_MVC_1P_Grupo_2_5544_.Models;
+
+namespace Proyecto_Web_MVC_1P_Grupo_2_5544_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Proyecto Web MVC 1P Grupo 2 5544_/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Web_MVC_1P_Grupo_2_5544_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
